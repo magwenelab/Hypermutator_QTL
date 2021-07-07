0cb3183d7ff0cc904048affefc0fa4a377ab5ac7
Cullen Roth <croth@localhost.localdomain>
2021-07-07 17:59:17 -0600

    Made edits to the read me file.

--- NOTEBOOKS/hypermutatorqtl.py
+++ NOTEBOOKS/hypermutatorqtl.py
@@ -18,18 +18,39 @@ colors = ['olive','salmon','tab:blue','tab:green',
           'tab:red','tab:grey','tab:brown',
           'lightgrey','tan','gold','lightblue']
 
-## SEt parnetal lables
+## Set parnetal lables
+## For H99
 h99_label = 'H99'+'$\mathrm{\u03B1}$'
+
+## Bt65 label
 bt_label =  r'Bt65$\bf{a}$'
 
 ## Define ftn for conducted a kruskal test
 def crypto_kruskal(site,pheno):
+    """
+    Conducts a bi-allelic comparison at SNP loci coded in SITE on phenotypes PHENO.
+    SITE: An array or list coded with either zero or one for a given SNP site.
+    PHENO: An array of list with phenotypic values for sample progeny.
+    This function assumes that the order of SITE and PHENO (left to right) are paired 
+    for each segregant.
+    """
     refxpheno = np.array(pheno,dtype=float)[np.array(site)==0]
     altxpheno = np.array(pheno,dtype=float)[np.array(site)==1]
     return -np.log10(ss.kruskal(refxpheno,altxpheno)[1])
 
 ## Define ftn for calculating CI
 def qtl_boot(segs,gt,df,pheno,pos='Pos',bs=1000,alpha=5):
+    """
+    Calculate 95 CI for QTL using a Kruskal-W H-test.
+    SEGS: labels of segregant names
+    GT: Genotype dataframe with rows represent SNP sites along a chromosome and columns represent progeny values.
+    DF: Phenotype dataframe with index of progeny names and columns containing phenotype values.
+    PHENO: Name of phenotype column in DF used in analysis.
+    POS: The positional column in the genotype dataframe containing the positon of a variant on a chromosome.
+    BS: Number of iterations used int he bootstraps.
+    ALPHA: The significance threshold used in calculating CI. Default is 5 representing an alpha of 0.05.
+    """
+    
     boot_straped = []
 
     while len(boot_straped)<bs:
